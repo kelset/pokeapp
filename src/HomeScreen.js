733a97adfa67661a59b789d506b4e3f4c84deb00
Lorenzo Sciandra <lorenzo.sciandra@gmail.com>
2019-08-01 14:03:35 +0100

    up to flatlst

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -3,14 +3,42 @@
  */
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
 
 import type { NavigationScreenProp } from 'react-navigation';
 
 type Props = {
   navigation: NavigationScreenProp<any>,
 };
+
+type Pokemon = {
+  number: number,
+  name: String,
+  classification: string,
+  image: string,
+  types: [string],
+};
+
+const PokemonsQuery = gql`
+  query pokemons {
+    pokemons(first: 40) {
+      number
+      name
+      classification
+      image
+      types
+    }
+  }
+`;
+
+const renderItem = (pokemon: { number: number, name: string }) => (
+  <View key={pokemon.number} style={{ backgroundColor: 'coral', margin: 30 }}>
+    <Text>{pokemon.name}</Text>
+  </View>
+);
 
 export function HomeScreen(props: Props): React$Node {
   return (
@@ -23,6 +51,25 @@ export function HomeScreen(props: Props): React$Node {
       >
         <Text style={styles.welcome}>Hello this is the home screen</Text>
       </TouchableOpacity>
+
+      <Query query={PokemonsQuery} style={{ flex: 1 }}>
+        {({
+          loading,
+          error,
+          data,
+        }: {
+          loading: boolean,
+          error: Object,
+          data: { pokemons: [Pokemon] },
+        }): React$Node => {
+          if (loading) return <Text>Loading...</Text>;
+          if (error) return <Text>Error :(</Text>;
+
+          console.log('hey here are my data', data.pokemons);
+          // $FlowFixMe
+          return <FlatList data={data.pokemons} renderItem={renderItem} />;
+        }}
+      </Query>
     </View>
   );
 }
@@ -30,8 +77,6 @@ export function HomeScreen(props: Props): React$Node {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   welcome: {
     fontSize: 20,
